d017696f8d8211d8c89efb2ba65732641c172efb
Ahmad Zaky Ubaidillah <ubaidillahzaky11@gmail.com>
2024-10-07 08:18:46 +0700

    update card ui

--- src/components/card-product.tsx
+++ src/components/card-product.tsx
@@ -6,16 +6,16 @@ const CardProduct = () => {
   const title = "kardus bekas kecap bango1ply - 59ml-33x21x11 original";
 
   return (
-    <Card className="flex flex-col gap-1 pb-2 text-wrap">
+    <Card className="flex flex-col gap-1 text-wrap pb-2">
       <Image src={KardusBekas} alt="gambar" />
-      <div className="space-y-1 px-1 text-sm text-wrap">
+      <div className="space-y-1 text-wrap px-2 text-sm">
         <p>{title.length > 30 ? title.slice(0, 30) + "..." : title}</p>
         <p className="text-yellow-400">Rp1.500</p>
         <div className="flex justify-between">
-          <p className="bg-primary px-2 border rounded-md text-background">
+          <p className="rounded-md border bg-primary px-2 text-background">
             +cart
           </p>
-          <p className="bg-primary px-2 border rounded-md text-background">
+          <p className="rounded-md border bg-primary px-2 text-background">
             buy now
           </p>
         </div>
